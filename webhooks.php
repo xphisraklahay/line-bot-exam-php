7fbda45594f7bd45f9bac6f5d70ab5b0a9a26e57
xphisraklahay <61489413+xphisraklahay@users.noreply.github.com>
2020-03-10 08:50:33 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 4G6fYrRvIq3SmwB3H+BSxj7kqfS3GZWXGvOxeuAY20zbcojVJCPOn+07Xwlnn2JgfuutobhdrLicznBg//oKH5JEA/wkR43qZGHkrhOJQmTYpc7deUtJA97ho0LBToPkVobF39rLZ1hO7JbJLHuEyAdB04t89/1O/w1cDnyilFU=#'3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
